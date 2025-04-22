4b864c38a6e52b5321411a0291173d0011349b1d
pramodambati <110030966+pramodambati@users.noreply.github.com>
2025-04-22 01:54:22 +0000

    conversions

--- 01_Basics/03_datatype_conversion.js
+++ 01_Basics/03_datatype_conversion.js
@@ -7,12 +7,12 @@ let score = true // 1 => number
 
 // const {score} = req.body
 
-console.log(typeof(score));
-console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);
-console.log(typeof valueInNumber); // pakka it's a number
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber); // pakka it's a number
 
 
 // "33" => 33
@@ -22,7 +22,7 @@ console.log(typeof valueInNumber); // pakka it's a number
 let isLoggedIn = " "
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log('isLoggedIN  '  + booleanIsLoggedIn);
+// console.log('isLoggedIN  '  + booleanIsLoggedIn);
 
 // 1 => true; 0 =>false
 // "" =>false
@@ -31,5 +31,57 @@ console.log('isLoggedIN  '  + booleanIsLoggedIn);
 let someNumber = 33
 
 let StringNumber = String(someNumber)
-console.log("stringNumber ", StringNumber);
-console.log("type of stringNumber ", typeof StringNumber);
+// console.log("stringNumber ", StringNumber);
+// console.log("type of stringNumber ", typeof StringNumber);
+
+
+/**************************Operations ***********************/
+
+let str1 = "hello"
+let str2 = "pramod"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+
+// console.log( (3 + 4) * 5 % 3);
+// console.log( 3 + 4 * 5 % 3); // where did you learn this messy code they will question.
+
+// console.log(true);
+// console.log(+true); //1
+// console.log(true+); // Unexpected token
+// console.log(+""); // 0 // INVESTIGATION STUDY
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // Why to write the confusion code
+
+let gameCounter = 100
+gameCounter++ // post fix
+++gameCounter // prefix
+console.log(gameCounter);
+
+/* 
+The increment (++) operator increments (adds one to) its operand 
+and returns the value before or after the increment, 
+depending on where the operator is placed. */
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
