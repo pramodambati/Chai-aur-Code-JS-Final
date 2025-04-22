b0b62b16b040e0a5423607f3af9e2cfd8e3a308e
pramodambati <110030966+pramodambati@users.noreply.github.com>
2025-04-22 03:56:31 +0000

    stack and heap

--- 01_Basics/05_datatypes_summary.js
+++ 01_Basics/05_datatypes_summary.js
@@ -50,11 +50,11 @@ const myFunction = function () {
     // In JavaScript, if a function doesn’t return anything, it implicitly returns undefined
 }
 
-console.log(typeof heros); // object
-console.log(typeof myObj); // object
-console.log(typeof myFunction); // function // Function Object
-console.log(myFunction)
-console.log(myFunction())
+// console.log(typeof heros); // object
+// console.log(typeof myObj); // object
+// console.log(typeof myFunction); // function // Function Object
+// console.log(myFunction)
+// console.log(myFunction())
 
 /*
 console.log(myFunction) logs the function itself.
@@ -62,5 +62,60 @@ console.log(myFunction) logs the function itself.
 console.log(myFunction()) logs what the function returns (which is undefined unless you explicitly return something).
 */
 
+
+
+// ++++++++++++++++++++++++
+
+// The stack is used for static memory allocation (fixed-size memory). Variables are stored directly with their values.
+
+// The heap is used for dynamic memory allocation (variable-size memory). (variables don’t hold the value directly, just a pointer to the heap).
+
+// Stack (Primitive), Heap (Reference)
+
+let myYoutubeName = "hiteshchoudharydotcom"
+let anotherName = myYoutubeName
+
+anotherName = "Chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+
+const user1 = {
+    name : "pramod",
+    upi : "939@ybl"
+}
+
+const user2 = user1
+// This is reassigning the values not allowed when constand
+// user2 = {
+//     name : "Reddy",
+//     upi : "800ybl"
+// }
+
+user2.name = "Reddy"
+
+console.log(user1);
+console.log(user2);
+
+/* 
+
+Your code works because:
+
+const only prevents reassigning user1 and user2 to new objects.
+
+It does not prevent modifying the existing object's properties.
+
+Since user1 and user2 reference the same object, changing user2.name also changes user1.name.
+
+For Objects (and Arrays)
+const only prevents reassigning the variable, not modifying the object's properties.
+
+const user = { name: "Pramod" };
+user.name = "Reddy"; // ✅ Allowed (modifying the object's property)
+
+user = { name: "John" }; // ❌ Error (reassigning the variable)
+
+*/
 
 
