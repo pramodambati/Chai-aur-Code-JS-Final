51d706f9dadff03c1b1540e36c62e19642f132e2
pramodambati <110030966+pramodambati@users.noreply.github.com>
2025-05-17 06:01:51 +0000

    shopping cart function

---
+++ 03_Basics/02_functions_shopingCart.js
@@ -0,0 +1,38 @@
+// Shoping cart you don't know how many parameters will come
+
+function calculateCartPrice(...num1) {
+    return num1     
+}
+
+// console.log(calculateCartPrice(1, 2, 3));
+
+
+function calculateCartPrice2(val1, val2, ...num1) {
+    // return num1 , val1    
+    return [num1 , val1 ]   
+}
+
+// console.log(calculateCartPrice2(1, 2, 3, 4, 5));
+
+const user = {
+    username : "hitesh",
+    prices : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.prices}`);  
+}
+
+handleObject(user)
+
+handleObject({
+    username : "pramod",
+    prices : 999,
+    rating : 5
+})
+
+function returnSecondValue(anyarray) {
+    console.log(anyarray[1]);    
+}
+
+returnSecondValue([1, 'pramod', 2])
