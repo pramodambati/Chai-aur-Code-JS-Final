27c8827959359e9bbb2f49382d9cb73a6f797dd3
pramodambati <110030966+pramodambati@users.noreply.github.com>
2025-05-19 17:21:01 +0000

    if else control flow

---
+++ 04_control_flow/01_if_else.js
@@ -0,0 +1,47 @@
+// Control flow or logic flow
+
+if ( 2 == '2') {
+    // console.log("Executed");
+    
+}
+
+// Conditional Checks : <, >, <=, >=, ==, !=, ===, !==
+
+const temperature = 41
+
+if (temperature < 50) {
+    // console.log("Less than 50");
+} else {
+    // console.log("temperature is greater than 50");
+}
+
+// console.log("Always Execute");
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    // console.log(`User power : ${power}`);
+}
+
+// console.log(`User power : ${power}`);
+
+// Short hand notaion
+
+const balance = 1000 
+
+// if (balance > 500) console.log("test"),
+// console.log("test 2");
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+} else if (balance < 900) {
+    console.log("greater than 750 and less than 900");
+} else {
+    console.log("less than 1200");
+}
+
+
+
