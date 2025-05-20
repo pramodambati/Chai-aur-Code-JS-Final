086329edd6c2c5d531f9620af1bec38301f78d20
pramodambati <110030966+pramodambati@users.noreply.github.com>
2025-05-20 02:28:22 +0000

    while and do while loop

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,29 @@
+// While loop
+
+// while (2 == 2) {
+//     console.log("yes"); // when will you terminate??
+// }
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["ironman", "spiderman", "hulk"]
+
+let arr = 0
+
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+
+// let score = 1
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 5);
+
