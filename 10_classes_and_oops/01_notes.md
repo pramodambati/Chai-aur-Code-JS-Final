17f07285cb600e50395f800315be039180416ab0
pramodambati <110030966+pramodambati@users.noreply.github.com>
2025-06-06 09:50:50 +0000

    oops in js

--- 10_classes_and_oops/01_notes.md
+++ 10_classes_and_oops/01_notes.md
@@ -1,6 +1,6 @@
 # javascript and classes
 
-## js is primarily a prototype-based langauage
+## js is primarily a prototype-based langauage, classes are primarily syntatical-sugar over esxisting prototype-based inheritance mechanism
 
 ## Js does have classes, remember that they work somewhat differently from classes in languages with class-based inheritance, due to js prototype-based nature.
 
@@ -8,3 +8,19 @@
 - collection of properties and methods
 - toLowerCase, promise is also an object.
 
+
+## why use OOP
+
+## parts of OOP
+Object literal {}
+
+- Construtor function
+- Prototypes
+- Classes
+- Instances (new, this)
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ 10_classes_and_oops/02_oop.js
@@ -0,0 +1,59 @@
+// litrellay a object
+
+const user = {
+    username : "Pramod",
+    loginCount : 8,
+    signedIn : true,
+
+    getUserdetails : function () {
+        // console.log(`Got user details from DB ${this.username}`);
+        console.log(this);
+    }
+}
+
+
+// console.log(user["username"]);
+// console.log(user.loginCount);
+// user.getUserdetails()
+// console.log(this);
+
+
+// Constructor function
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+
+function User(username, loginCount, isLoggedIn) {
+    // myusername = username
+    this.username = username
+    this.logicloginCount =loginCount
+    this.isisLoggedIn =isLoggedIn
+
+    this.greeting = function () {
+        console.log(`Welcome ${this.greetingusername}`);
+    }
+    // return this // without explicitly mention also it will return
+}
+ 
+
+// const userOne = User("pramod", 5, true)
+// const usertwo = User("james", 15, false)
+// console.log(userOne); // it's overriding the userone values with usertwo values
+
+
+const userOne = new User("pramod", 5, true)
+const usertwo = new User("james", 15, false)
+// console.log(userOne); 
+// console.log(usertwo); 
+
+console.log(userOne.constructor); // Reference about it self
+
+// console.log(userOne instanceof usertwo);
+
+// check instanceof in mdn
+
+
+
+
+
